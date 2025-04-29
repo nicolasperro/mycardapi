28588cdc70554927906b02597e83bddbc24ab251
nicolasperro <tutscraftferb@gmail.com>
2025-04-29 08:14:47 -0300

    primeiro commit

---
+++ src/main/java/com/mycard/mycardapi/MycardapiApplication.java
@@ -0,0 +1,13 @@
+package com.mycard.mycardapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycardapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycardapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycard/mycardapi/MycardapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycard.mycardapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycardapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
