a0fb36781014ca9b8addcd629c5ed9e689165586
João Victor <jv05@outlook.pt>
2025-06-03 17:09:54 -0300

    Update AtletaRepository.java

--- src/main/java/com/mycard/mycardapi/model/repository/AtletaRepository.java
+++ src/main/java/com/mycard/mycardapi/model/repository/AtletaRepository.java
@@ -1,1 +1,10 @@
+package com.mycard.mycardapi.repository;
+
+import com.mycard.mycardapi.model.entity.Atleta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AtletaRepository extends JpaRepository<Atleta, Long> {
+    }
 
