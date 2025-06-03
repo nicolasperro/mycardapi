bd7896f128234f8f7ce2c79235d20d4d43ed0096
João Victor <jv05@outlook.pt>
2025-06-03 17:08:29 -0300

    Update ArbitroRepository.java

--- src/main/java/com/mycard/mycardapi/model/repository/ArbitroRepository.java
+++ src/main/java/com/mycard/mycardapi/model/repository/ArbitroRepository.java
@@ -1,1 +1,9 @@
+package com.mycard.mycardapi.repository;
 
+import com.mycard.mycardapi.model.entity.Arbitro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArbitroRepository extends JpaRepository<Arbitro, Long> {
+    }
