ed61695d54d495b1a149907f0f4c4ff42d8e360f
João Victor <jv05@outlook.pt>
2025-06-03 17:23:21 -0300

    Update FuncionarioRepository.java

--- src/main/java/com/mycard/mycardapi/model/repository/FuncionarioRepository.java
+++ src/main/java/com/mycard/mycardapi/model/repository/FuncionarioRepository.java
@@ -1,1 +1,15 @@
+package com.mycard.mycardapi.repository;
+
+import com.mycard.mycardapi.model.entity.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+
+    Optional<Funcionario> findByCpf(String cpf);
+    Optional<Funcionario> findByEmailContato(String emailContato);
+}
 
