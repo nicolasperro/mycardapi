db3996a8eccd216d91c6b036d9adc7509bd726bd
João Victor <jv05@outlook.pt>
2025-06-03 17:31:22 -0300

    Update ModalidadeRepository.java

--- src/main/java/com/mycard/mycardapi/model/repository/ModalidadeRepository.java
+++ src/main/java/com/mycard/mycardapi/model/repository/ModalidadeRepository.java
@@ -1,1 +1,14 @@
+package com.mycard.mycardapi.repository;
+
+import com.mycard.mycardapi.model.entity.Modalidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ModalidadeRepository extends JpaRepository<Modalidade, Long> {
+
+    Optional<Modalidade> findByNomeModalidade(String nomeModalidade);
+}
 
