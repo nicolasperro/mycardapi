42c14488d921587ab62398410b837df687222992
João Victor <jv05@outlook.pt>
2025-06-03 17:15:11 -0300

    Update EquipeRepository.java

--- src/main/java/com/mycard/mycardapi/model/repository/EquipeRepository.java
+++ src/main/java/com/mycard/mycardapi/model/repository/EquipeRepository.java
@@ -1,1 +1,10 @@
 
+package com.mycard.mycardapi.repository;
+
+import com.mycard.mycardapi.model.entity.Equipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EquipeRepository extends JpaRepository<Equipe, Long> {
+     }
