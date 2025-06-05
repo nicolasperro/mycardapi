628fcaca8651b010d308c502960c99378b497368
João Victor <jv05@outlook.pt>
2025-06-05 17:42:56 -0300

    Update EventoService.java

--- src/main/java/com/mycard/mycardapi/service/EventoService.java
+++ src/main/java/com/mycard/mycardapi/service/EventoService.java
@@ -1,1 +1,39 @@
+package com.mycard.mycardapi.service;
+
+import com.mycard.mycardapi.exception.RegraNegocioException;
+import com.mycard.mycardapi.model.entity.Evento;
+import com.mycard.mycardapi.model.repository.EventoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EventoService {
+
+    private final EventoRepository repository;
+
+    public EventoService(EventoRepository repository) {
+        this.repository = repository;
+    }
+
+    public Evento salvar(Evento evento) {
+        return repository.save(evento);
+    }
+
+    public Optional<Evento> getEventoById(Long id) {
+        return repository.findById(id);
+    }
+
+    public List<Evento> listarTodos() {
+        return repository.findAll();
+    }
+
+    public void deletar(Long id) {
+        if (!repository.existsById(id)) {
+            throw new RegraNegocioException("Evento não encontrado");
+        }
+        repository.deleteById(id);
+    }
+}
 
