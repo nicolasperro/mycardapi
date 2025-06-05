0722a0cbfd9d2440b3ca7c45bb51ae32a83d2207
João Victor <jv05@outlook.pt>
2025-06-05 16:08:05 -0300

    Update OrganizacaoArbitragemRepository.java

--- src/main/java/com/mycard/mycardapi/model/repository/OrganizacaoArbitragemRepository.java
+++ src/main/java/com/mycard/mycardapi/model/repository/OrganizacaoArbitragemRepository.java
@@ -1,4 +1,4 @@
-package com.mycard.mycardapi.repository;
+package com.mycard.mycardapi.model.repository;
 
 import com.mycard.mycardapi.model.entity.OrganizacaoArbitragem;
 import org.springframework.data.jpa.repository.JpaRepository;
