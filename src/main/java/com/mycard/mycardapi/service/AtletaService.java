d4e22e661ee12f9447807daf636544cfa36aa53a
João Victor <jv05@outlook.pt>
2025-06-05 17:28:54 -0300

    Update AtletaService.java

--- src/main/java/com/mycard/mycardapi/service/AtletaService.java
+++ src/main/java/com/mycard/mycardapi/service/AtletaService.java
@@ -1,1 +1,39 @@
+package com.mycard.mycardapi.service;
+
+import com.mycard.mycardapi.exception.RegraNegocioException;
+import com.mycard.mycardapi.model.entity.Atleta;
+import com.mycard.mycardapi.model.repository.AtletaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AtletaService {
+
+    private final AtletaRepository repository;
+
+    public AtletaService(AtletaRepository repository) {
+        this.repository = repository;
+    }
+
+    public Atleta salvar(Atleta atleta) {
+        return repository.save(atleta);
+    }
+
+    public Optional<Atleta> getAtletaById(Long id) {
+        return repository.findById(id);
+    }
+
+    public List<Atleta> listarTodos() {
+        return repository.findAll();
+    }
+
+    public void deletar(Long id) {
+        if (!repository.existsById(id)) {
+            throw new RegraNegocioException("Atleta não encontrado");
+        }
+        repository.deleteById(id);
+    }
+}
 
