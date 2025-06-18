81b35c287dacbb5e48540f73850521a2203fa800
João Victor <jv05@outlook.pt>
2025-06-18 16:56:08 -0300

    Delete src/main/java/com/mycard/mycardapi/model/repository directory

--- src/main/java/com/mycard/mycardapi/model/repository/ArbitroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mycard.mycardapi.model.repository;
-
-import com.mycard.mycardapi.model.entity.Arbitro;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ArbitroRepository extends JpaRepository<Arbitro, Long> {
-    }
--- src/main/java/com/mycard/mycardapi/model/repository/EquipeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mycard.mycardapi.model.repository;
-
-import com.mycard.mycardapi.model.entity.Equipe;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EquipeRepository extends JpaRepository<Equipe, Long> {
-     }
