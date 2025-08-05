169d32669571895fae9d52c72906cb53778efec4
Eduardo Castro <eduardocpcastrosilva@gmail.com>
2025-08-05 17:19:29 -0300

    Update LutaDTO.java

--- src/main/java/com/mycard/mycardapi/api/dto/LutaDTO.java
+++ src/main/java/com/mycard/mycardapi/api/dto/LutaDTO.java
@@ -12,7 +12,6 @@ import org.modelmapper.ModelMapper;
 public class LutaDTO {
     private Long id;
 
-    // IDs
     private Long idEvento;
     private Long idAtleta1;
     private Long idAtleta2;
@@ -21,7 +20,6 @@ public class LutaDTO {
     private Long idArbitro;
     private Long idOrganizacao;
 
-    // Dados para exibição
     private String evento;
     private String atleta01;
     private String atleta02;
@@ -36,47 +34,39 @@ public class LutaDTO {
         LutaDTO dto = new LutaDTO();
         dto.setId(luta.getId());
 
-        // Evento
         if (luta.getEvento() != null) {
             dto.setIdEvento(luta.getEvento().getId());
             dto.setEvento(luta.getEvento().getNomeEvento());
         }
 
-        // Atleta 01
         if (luta.getAtleta01() != null) {
             dto.setIdAtleta1(luta.getAtleta01().getId());
             dto.setAtleta01(luta.getAtleta01().getNomeCompleto());
         }
 
-        // Atleta 02
         if (luta.getAtleta02() != null) {
             dto.setIdAtleta2(luta.getAtleta02().getId());
             dto.setAtleta02(luta.getAtleta02().getNomeCompleto());
         }
 
-        // Vencedor
         if (luta.getAtletaVencedor() != null) {
             dto.setIdVencedor(luta.getAtletaVencedor().getId());
             dto.setAtletaVencedor(luta.getAtletaVencedor().getNomeCompleto());
         }
 
-        // Arbitro
         if (luta.getArbitro() != null) {
             dto.setIdArbitro(luta.getArbitro().getId());
         }
 
-        // Modalidade
         if (luta.getModalidade() != null) {
             dto.setIdModalidade(luta.getModalidade().getId());
             dto.setModalidade(luta.getModalidade().getNomeModalidade());
         }
-
-        // Organização de Arbitragem
+        
         if (luta.getOrganizacaoArbitragem() != null) {
             dto.setIdOrganizacao(luta.getOrganizacaoArbitragem().getId());
         }
 
-        // Outros campos simples
         dto.setPesoCategoria(luta.getPesoCategoria());
         dto.setRounds(luta.getRounds());
         dto.setRoundEncerramento(luta.getRoundEncerramento());
